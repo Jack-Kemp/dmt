065f3f4ec4afa21efa7d807afdd0a9c851ba2a5d
Jack Kemp <jkemp@holylogin02.rc.fas.harvard.edu>
2021-08-09 18:24:32 -0400

    Checkpoint redirect scripts for examples

---
+++ examples/checkpoint_glob.py
@@ -0,0 +1,14 @@
+import sys
+sys.path.append('/n/home06/jkemp/SharedDMT')
+import checkpoint_glob
+
+
+
+def main(argv):
+    """A redirect script to the main checkpoint_glob_all in SharedDMT
+    folder.
+    """
+    checkpoint_glob.main(["",argv[1]])
+
+if __name__ == "__main__":
+    main(sys.argv)
